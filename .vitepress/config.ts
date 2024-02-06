88c75daab4ea2ad62c056bae02f5db93cdb48a24
ryu <114303361+ryuapp@users.noreply.github.com>
2024-02-06 22:53:34 +0900

    docs: Twitter to X

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -200,7 +200,7 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/honojs' },
       { icon: 'discord', link: 'https://discord.gg/KMh2eNSdxV' },
-      { icon: 'twitter', link: 'https://twitter.com/honojs' },
+      { icon: 'x', link: 'https://twitter.com/honojs' },
     ],
     editLink: {
       pattern: 'https://github.com/honojs/website/edit/main/:path',
