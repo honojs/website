3f3d9086e06ceb17553d23c337a3463a4a5c9649
Kengo Watanabe <121782456+nabeken5@users.noreply.github.com>
2024-03-06 22:00:01 +0900

    feat: use tailwindcss

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -7,7 +7,11 @@ export default jsxRenderer(({ children, title, frontmatter }) => {
         <meta charset='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         {<title>{title ?? frontmatter?.title ?? 'My Blog'}</title>}
-        <link rel='stylesheet' href='/static/style.css' />
+        {import.meta.env.PROD ? (
+          <link href='static/assets/style.css' rel='stylesheet' />
+        ) : (
+          <link href='/app/style.css' rel='stylesheet' />
+        )}
       </head>
       <body>
         <header>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
