363239109e79119f706267005c7bf203567e4c12
Kengo Watanabe <121782456+nabeken5@users.noreply.github.com>
2024-03-06 23:28:21 +0900

    style: edit style

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,6 +1,14 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 
 export default jsxRenderer(({ children, title, frontmatter }) => {
+  const proseClass = [
+    "prose px-6 pb-24 pt-8",
+    "prose-h2:pt-6 prose-h2:border-t",
+    "prose-pre:-mx-6 prose-pre:!bg-[rgb(246,_248,_250)] prose-pre:!rounded-none prose-pre:px-6 prose-pre:py-5",
+    "prose-code:min-w-fit prose-code:inline prose-code:[&>pre]:block prose-code:before:!content-none prose-code:after:!content-none prose-code:px-2",
+    "prose-li:my-0",
+    "prose-a:text-[#e36002]"
+  ].join(" ")
   return (
     <html lang='en'>
       <head>
@@ -19,9 +27,10 @@ export default jsxRenderer(({ children, title, frontmatter }) => {
             <a href='/'>My Blog</a>
           </h1>
         </header>
-        <main class={frontmatter?.title ? "prose" : undefined}>
+        <main class={frontmatter?.title ? proseClass : undefined}>
           {children}
         </main>
+        <br class="prose-l prose-a"/>
         <footer>
           <p>&copy; 2024 My Blog. All rights reserved.</p>
         </footer>
