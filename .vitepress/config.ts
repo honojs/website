af3466c62c01ef4f15f3ca8b2bd6cc70e8243ae3
-akku- <151517253+akku1139@users.noreply.github.com>
2024-04-27 22:18:38 +0900

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -214,7 +214,7 @@ export default defineConfig({
     },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/honojs' },
-      { icon: 'discord', link: 'https://discord.gg/KMh2eNSdxV' },
+      { icon: 'discord', link: 'https://discord.gg/VARKtGvCZK' },
       { icon: 'x', link: 'https://twitter.com/honojs' },
     ],
     editLink: {
