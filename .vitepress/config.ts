889f6b66b738f66ae0911f3dd3c6f563d7018785
Max Gerber <max@stytch.com>
2025-06-15 08:49:49 -0700

    docs(stytch-auth): Add Integration example with Stytch Authentication

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -324,6 +324,10 @@ export const sidebarsExamples = (): DefaultTheme.SidebarItem[] => [
         text: 'Pylon (GraphQL)',
         link: '/examples/pylon',
       },
+      {
+        text: 'Stytch Authentication',
+        link: '/examples/stytch-auth',
+      },
     ],
   },
 ]
